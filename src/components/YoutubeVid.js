c4d201dfad0f4d3485062521e34a565662302d26
Lizzie Paquette <33362730+lizziepaquette@users.noreply.github.com>
2022-03-06 11:12:45 -0800

    Initialize eslint

---
+++ src/components/YoutubeVid.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const YoutubeVid = () => (
+    <div>
+    <iframe width="100%"
+        height="315"
+        src="https://www.youtube.com/embed/Kktx2ph4UYc"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen="true">
+    </iframe>
+    </div>
+)
+
+export default YoutubeVid
