7cd771d30cf3df5cf85e8c6c43834bb4ae34971c
Lizzie Paquette <lizzie@brex.com>
2022-03-06 22:31:12 -0800

    schedule details

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -37,43 +37,67 @@ const Schedule = () => {
       title: 'Dolores Park',
       content: <div>
         <div> 2pm - 4pm </div>
-        <div> Even though we dont hang out at Dolores much, we picked it because of its central location </div>
+        <div>
+          We dont hang at Dolores much (it&apos;s too &quot;crowded&quot; for Jack :-/), but we&apos;ve spent a lot of time
+          over the last 3 years chilling in parks and its location
+          (southern-most stop on our tour, close to 16th-Mission BART) makes it the day&apos;s perfect starting point
+        </div>
       </div>
     },
     {
       title: 'Bar Part Time',
       content: <div>
             <div> 4pm - 6pm </div>
-            <div> Wine bar with disco ball </div>
+            <div>
+              3 years ago, our friend Thomas showed us Ruby Wine, and it was love-at-first-sip-on-Potrero-Hill (or something like that).
+              Ruby is a little far to include in our tour, but Thomas has since showed us Bar Part Time - a magical venue sharing Ruby&apos;s
+              regulars, but inside with a disco ball.
+            </div>
         </div>
     },
     {
-      title: 'Little Star Pizza',
+      title: 'Little Star Pizza/Dancing Yak',
       content: <div>
             <div> 6pm - 7pm </div>
-            <div> Vegan Cheese Pizza Place </div>
-            <div> Horse winery tour at 6pm </div>
-            <div> Steaktips, corn, and salad by Chef Kate 8pm </div>
+            <div>
+              We relied on Little Star&apos;s deep dish a solid bit during the pandemic. They have Vegan Cheese that Lizzie and Jack approve of,
+              so our stomachs can all rest a little easier after this stop.
+              <br></br>
+              <br></br>
+              Feeling Nepali? Dancing Yak is another one of our faves and it&apos;s up half a block from Little Star! Jack once stole an ice cube
+              out of Lizzie&apos;s mouth while eating their Butter Chicken because it was so spicy...
+            </div>
         </div>
     },
     {
       title: '300 Buchanan st',
       content: <div>
             <div> 7pm - 9pm  </div>
-            <div> Tiki Happy Hour 4pm - 6pm </div>
+            <div>
+              A chance to see our digs one final time and make cocktails from pre-selected recipes.
+              You&apos;ll need the cocktails to get through Jack sobbing and pointing out mundane
+              objects in the southern San Francisco landscape for the full 2 hours.
+            </div>
         </div>
     },
         {
       title: 'Church of 8 Wheels',
       content: <div>
             <Bdiv> 9pm - 11pm  </Bdiv>
-            <Bdiv> Pool it dude 1pm - 4:30pm </Bdiv>
+            <div>
+              The entree to end all entrees - the incomparable Church of 8 Wheels. Watch Lizzie
+              zoom around the church while the rest of us absorb the funk and admire the magical
+              weaving of skaters deep into the Fillmore night.
+            </div>
           </div>
     },
         {
       title: 'Karaoke',
       content: <div>
           <div > 11pm - 2am  </div>
+          <div>
+            I highly doubt I&apos;ll be awake for this.
+          </div>
         </div>
     }
   ]
